1daa7a82bac08f64662dc0f51ce6a2e31646eea4
Ken9709 <Kwood9709@gmail.com>
2022-09-21 21:03:25 -0700

    backend update for read, update, delete functionality

--- graphql/resolvers/tickets.js
+++ graphql/resolvers/tickets.js
@@ -4,7 +4,9 @@ const Ticket = require('../../models/Ticket');
 
 module.exports = {
     Mutation: { 
-        async createTicket(_, {ticketInput:{content,isAsigned}}){
+        async createTicket(_, {ticketInput:{content, isAsigned}}){
+            //create a new model based on the mongoose model we already made.
+            //we are SETTING UP THE MODEL, not saving data.
             const newTicket = new Ticket({
                 
                 content: content,
@@ -14,16 +16,37 @@ module.exports = {
                 
             });
             
+            //save the newly created model to mongoDB
             const res = await newTicket.save();
-            console.log(res);
+            console.log(res._doc)
             return{
                 id:res.id,
                 //rest of attributes necessary for return statement
+            
                 ...res._doc
+                
             };
+        },
+        async deleteTicket(_, {ID}) {
+            //delete a ticket based on its ID, compared to the implicit mongoDB id
+            const wasDeleted = (await Ticket.deleteOne({_id: ID})).deletedCount;
+            return wasDeleted; //deletedCount gives 1 if something deleted, 0 if not
+        },
+
+        async editTicket(_, {ID, ticketInput:{content, isAsigned}}){
+            const wasEdited = (await Ticket.updateOne({ _id: ID}, {content: content, isAsigned: isAsigned})).modifiedCount;
+            return wasEdited; 
         }
     },
     Query: {
-        ticket: (_, {ID}) => Ticket.findById(ID)
+        // first arguement is the parent( dont need), second is parameters being passed into resolver
+        // destrcutured, 
+        async ticket (_, {ID}) {
+            return await Ticket.findById(ID)
+        },
+        async getTickets(_, {amount}) {
+            //find the tickets created last and limit them by the amount(gives {amount} oldest tickets)
+            return await Ticket.find().sort({createdAt: -1}).limit(amount)
+        }
     }
 }
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -44,18 +44,28 @@ input LoginInput{
     email: String,
     password: String
 }
+
 type Query{
     message(id: ID!): Message,
     user(id: ID!): User,
-    ticket(id: ID!): Ticket
+    ticket(id: ID!): Ticket,
+    getTickets(amount: Int): [Ticket]
 }
 
 type Mutation { 
     createMessage(messageInput: MessageInput): Message!,
     registerUser(registerInput: RegisterInput): User,
     loginUser(loginInput: LoginInput): User,
-    createTicket(ticketInput: TicketInput): Ticket
+    createTicket(ticketInput: TicketInput): Ticket,
+    deleteTicket(ID: ID!): Boolean
+    editTicket(ID: ID!, ticketInput: TicketInput): Boolean
 }
 
 `
 
+// we could create a new input type like EditTicketInput like this
+// Which would not allow the users to change the content, only the isaAssigned variable, etc
+// editTicket(ID: ID!, editTicketInput: EditTicketInput)
+// input EditTicketInput{
+//    content:String
+// }
